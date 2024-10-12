4cacaaa7a8edf8ebc3a82b3382c1a9e9098e45e0
Botito <107950141+botjumbo@users.noreply.github.com>
2024-10-12 00:04:16 -0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -605,7 +605,7 @@ function loadChartData(input) {
     const inputUpperCase = input; // 
 
     // Actualizar el título del gráfico
-    document.getElementById('instrument-title').textContent = `Análisis de ${inputUpperCase.replace('.CSV', '')}`;
+    document.getElementById('instrument-title').textContent = `Análisis de ${inputUpperCase.replace('.csv', '')}`;
 
     console.log(`Instrumento actual: ${inputUpperCase.replace('.csv', '')}`); // Verifica el valor sin '.CSV'
 
