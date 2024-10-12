e532dbc5540eb236d964361b42d42e349d13a95f
Botito <107950141+botjumbo@users.noreply.github.com>
2024-10-12 20:58:53 -0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1,5 +1,5 @@
-const symbols = ['AL30', 'AL30D','GD30', 'GD30D','AE38', 'AE38D','AL30C', 'AL35','AL35D', 'GD30C','GD35', 'GD35D','MERVAL', 'TX26','TX28'];
-const symbol = symbols.map(s => s + '.csv');
+const symbol = ['AL30', 'AL30D','GD30', 'GD30D','AE38', 'AE38D','AL30C', 'AL35','AL35D', 'GD30C','GD35', 'GD35D','MERVAL', 'TX26','TX28'];
+//const symbol = symbols.map(s => s + '.csv');
 const promises = symbol.map(file => loadCSV(`/ratios-argy/${file}`));
 const legendElement = document.getElementById('legend');
 const suggestions = document.getElementById('suggestions');
