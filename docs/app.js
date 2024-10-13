e04c65c5a08b8ba47d58bf6937cd24c104c98a97
Botito <107950141+botjumbo@users.noreply.github.com>
2024-10-13 20:30:13 -0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -906,20 +906,28 @@ function toggleChartType(isRatio = false) {
     let dataToUse = isRatio ? ratioData : formattedData; // Usar ratioData o formattedData según corresponda
     const chartTypeText = isLineChart ? "Mostrar Gráfico de Línea" : "Mostrar Gráfico de Velas";
 
+    // Limpieza de datos antes de establecer nuevos
+    candleSeries.setData([]); // Limpiar datos de velas
+    lineSeries.setData([]); // Limpiar datos de línea
+
+    if (dataToUse.length === 0) {
+        console.warn("No hay datos para mostrar.");
+        return; // Salir si no hay datos
+    }
+
     if (isLineChart) {
         // Cambiar a gráfico de velas
         candleSeries.setData(dataToUse); // Establecer datos para el gráfico de velas
-        lineSeries.setData([]); // Limpiar datos de línea
     } else {
         // Cambiar a gráfico de línea
         const lineData = convertCandleToLineSeries(dataToUse); // Convertir datos a serie de línea
         lineSeries.setData(lineData); // Establecer datos de línea
-        candleSeries.setData([]); // Limpiar datos de velas
     }
 
     document.getElementById('toggle-chart').innerText = chartTypeText; // Actualizar el texto del botón
     isLineChart = !isLineChart; // Alternar el estado del gráfico
 }
+
 function updateChart() {
     
     // Si hay un símbolo seleccionado
