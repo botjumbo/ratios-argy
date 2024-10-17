6dce25ea514068a024bdfb2065a9e481470d40f7
Botito <107950141+botjumbo@users.noreply.github.com>
2024-10-17 14:58:03 -0300

    Update app.js

--- docs/app.js
+++ docs/app.js
@@ -1011,7 +1011,7 @@ function toggleChartType(isRatio = false) {
 
     document.getElementById('toggle-chart').innerText = chartTypeText; // Actualizar el texto del botón
     isLineChart = !isLineChart; // Alternar el estado del gráfico
-    updateChart();
+    //updateChart();
     
 }
 
@@ -1032,9 +1032,9 @@ function updateChart() {
                     console.error('Ambos símbolos deben estar definidos antes de hacer la solicitud.');
                     return; // Detener ejecución si hay un símbolo indefinido
                 }
-                fetchAndUpdateChartData(divisionValues); // Llama a la función con el símbolo seleccionado
+                //fetchAndUpdateChartData(divisionValues); // Llama a la función con el símbolo seleccionado
 
-                //fetchAndUpdateChartDataRatio(symbol1, symbol2);
+                fetchAndUpdateChartDataRatio(symbol1, symbol2);
                 console.log(`${symbol1}/${symbol2} instrumentos seleccionados.`);
                 
             } else {
